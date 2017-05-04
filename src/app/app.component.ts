7d39358db59addeb3202924bfa5e362092d1232a
Ala08 <ala.marzouk@supinfo.com>
2017-05-04 23:17:36 +0200

    navbar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-
+import {navBarComponent} from './components/navbar/navbar.component';
 @Component({
+  moduleId:module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl: `app.component.html`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  {  }
